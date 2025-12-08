e929bb7563588f54f55e8bc6bd3960d674745cc8
dahyeon <youlove7r@gmail.com>
2025-12-08 23:30:36 +0900

    initial commit

---
+++ src/main/java/com/eateum/eateumbe/EateumBeApplication.java
@@ -0,0 +1,13 @@
+package com.eateum.eateumbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EateumBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EateumBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eateum/eateumbe/EateumBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eateum.eateumbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EateumBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
