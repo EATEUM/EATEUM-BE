f3950aa5ec3b7d071fcfa592160953b4814cb79e
minijee <ssjm5305@gmail.com>
2025-12-18 02:18:18 +0900

    Refactor: RefreshToken
    Cookie 기반 RefreshToken 회전 로직 서비스에 반영
    
    Ref: #49

--- src/main/java/com/eateum/eateumbe/user/service/UserService.java
+++ src/main/java/com/eateum/eateumbe/user/service/UserService.java
@@ -4,13 +4,14 @@ import com.eateum.eateumbe.user.domain.User;
 import com.eateum.eateumbe.user.dto.request.LoginRequest;
 import com.eateum.eateumbe.user.dto.response.LoginResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface UserService {
 
     //로그인
-    LoginResponse login(LoginRequest request);
+    LoginResponse login(LoginRequest request, HttpServletResponse response);
     
     //엑세스토큰 재발행
-//    LoginResponse reissue(HttpServletRequest request);
+//    LoginResponse reissue(String refreshToken, HttpServletResponse response);
 
 }
