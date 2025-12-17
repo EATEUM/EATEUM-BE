7d3f0d72818296d1b2553c2ee0099b84d926d09f
minijee <ssjm5305@gmail.com>
2025-12-17 16:02:15 +0900

    Feat: 리프레시 토큰 사용 위한 Service 추가
    Ref: #1

--- src/main/java/com/eateum/eateumbe/global/redis/RefreshTokenService.java
+++ src/main/java/com/eateum/eateumbe/global/redis/RefreshTokenService.java
@@ -1,4 +1,44 @@
 package com.eateum.eateumbe.global.redis;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
 public class RefreshTokenService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+    
+    //Refresh Token 유효기간 - 14일
+    private static final Duration REFRESH_TOKEN_TTL = Duration.ofDays(14);
+    
+    private static final String PREFIX = "refresh:";
+
+    /**
+     * refresh Token 저장
+     * key: refresh:{userId}
+     * value: refreshToken
+     */
+    public void save(String userId, String refreshToken) {
+        redisTemplate.opsForValue()
+                .set(PREFIX + userId, refreshToken);
+    }
+
+    /**
+     * refreshToken 조회
+     */
+    public String find(String userId) {
+        return redisTemplate.opsForValue()
+                .get(PREFIX + userId);
+    }
+
+    /**
+     * 로그아웃 시 Refresh Token 삭제
+     */
+    public void delete(String userId) {
+        redisTemplate.delete(PREFIX + userId);
+    }
 }
