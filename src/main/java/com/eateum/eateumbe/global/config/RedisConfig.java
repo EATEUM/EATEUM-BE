261e02ea9e6e5011660189535072f0b024d37f5e
minijee <ssjm5305@gmail.com>
2025-12-17 16:00:38 +0900

    Feat: 로그인
    레디스 설정파일 추가
    
    Resolves: #3
    Ref: #1

--- src/main/java/com/eateum/eateumbe/global/config/RedisConfig.java
+++ src/main/java/com/eateum/eateumbe/global/config/RedisConfig.java
@@ -1,4 +1,41 @@
 package com.eateum.eateumbe.global.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@Configuration
 public class RedisConfig {
+
+    /**
+     * application.yml의 spring.redis.* 설정을 자동으로 읽어 Redis 연결 팩토리 생성
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    /**
+     * Redis를 String 기반으로 사용하기 위한 Template
+     * key: String
+     * value: String (JWT, UUID 등)
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(
+            RedisConnectionFactory connectionFactory
+    ) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        //key, value를 사람이 읽을 수 있는 문자열로 저장
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
 }
