0d6be88b594c6d2fc5d312cd0646b301fbed3a07
minijee <ssjm5305@gmail.com>
2025-12-17 21:58:13 +0900

    Feat: 로그인 처리를 위한 UserMapper DAO 인터페이스 추가
    Ref: #1

--- src/main/java/com/eateum/eateumbe/user/repository/UserMapper.java
+++ src/main/java/com/eateum/eateumbe/user/repository/UserMapper.java
@@ -1,4 +1,12 @@
 package com.eateum.eateumbe.user.repository;
 
-public class UserMapper {
+import com.eateum.eateumbe.user.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+    //로그인
+    User findByEmail(String email);
+
 }
