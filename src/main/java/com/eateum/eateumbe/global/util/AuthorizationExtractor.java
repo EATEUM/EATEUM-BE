a82655cc93592bb3786c30b9a055766ba81f1585
minijee <ssjm5305@gmail.com>
2025-12-17 22:02:48 +0900

    Feat: 로그인
    Authorization 헤더에서 Bearer 토큰 추출 유틸 추가
    
    Resolves: #3
    Ref: #1

--- src/main/java/com/eateum/eateumbe/global/util/AuthorizationExtractor.java
+++ src/main/java/com/eateum/eateumbe/global/util/AuthorizationExtractor.java
@@ -1,4 +1,21 @@
 package com.eateum.eateumbe.global.util;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class AuthorizationExtractor {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    //유틸 클래스는 new 불가
+    private AuthorizationExtractor(){}
+
+    /**
+     * Authorization 헤더에서 토큰 추출
+     */
+    public static String extractBearearToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if(header == null) return null;
+        if(!header.startsWith(BEARER_PREFIX)) return null;
+        return header.substring(BEARER_PREFIX.length());
+    }
 }
