8c19ba3fcbeff1d20776261ca662b5e0e898e665
minijee <ssjm5305@gmail.com>
2025-12-19 16:18:01 +0900

    Feat: 프로필 수정
    프로필 수정 요청 DTO 및 User 도메인 Setter 추가
    
    Resolves: #8
    Ref: #1

---
+++ src/main/java/com/eateum/eateumbe/user/dto/request/UpdateInfoRequest.java
@@ -0,0 +1,15 @@
+package com.eateum.eateumbe.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateInfoRequest {
+
+    private String name;
+    //    private String profileImage; //프로필이미지 파일은 MultipartFile로만 처리
+
+}
