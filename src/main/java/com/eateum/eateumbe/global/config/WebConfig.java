40194d700e73de7c0ed442f3abc7361f77678d3d
kimgemini <rlaalswo2437@gmail.com>
2025-12-24 18:19:11 +0900

    Fix: Cors 에러 수정

--- src/main/java/com/eateum/eateumbe/global/config/WebConfig.java
+++ src/main/java/com/eateum/eateumbe/global/config/WebConfig.java
@@ -1,9 +1,14 @@
 package com.eateum.eateumbe.global.config;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -23,5 +28,17 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Component
+    public class PnaFilter implements Filter {
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+            HttpServletResponse res = (HttpServletResponse) response;
+            // 브라우저에게 프라이빗 네트워크 접근이 괜찮다고 알려줌
+            res.setHeader("Access-Control-Allow-Private-Network", "true");
+            chain.doFilter(request, response);
+        }
     }
 }
